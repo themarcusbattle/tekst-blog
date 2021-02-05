0cec2b5be5fdb308b22d4a67bfb7eaa291235d0c
Marcus Battle <marcus@marcusbattle.com>
2021-02-05 06:52:49 +0000

    Added function parse topic slug

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -10,4 +10,9 @@ class TopicController extends Controller
     {
     	echo $topic;
     }
+    
+    public function showContent(Request $request, $topic, $slug)
+    {
+    	echo $topic . ' ' . $slug;
+    }
 }
