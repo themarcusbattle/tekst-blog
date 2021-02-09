f8a83afbf5de014d1e723ebdf2bf9037e1b6b7cb
Marcus Battle <themarcusbattle@gmail.com>
2021-02-08 23:29:54 -0500

    Added home view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Topix</title>
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    </head>
+    <body class="p-3">
+        <h1 id="blog-title" class="text-xl mb-4">{{ $blog['blog_title'] }}</h1>
+        <p id="blog-description" class="text-sm text-gray-500">{{ $blog['blog_description'] }}</p>
+    </body>
+</html>
