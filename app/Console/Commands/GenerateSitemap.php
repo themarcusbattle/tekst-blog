1c236a37e0c57bcb0a613f0b068d346fef3784e0
Marcus Battle <themarcusbattle@gmail.com>
2021-02-14 00:29:31 -0500

    Added sitemap generator

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Sitemap\SitemapGenerator;
+
+class GenerateSitemap extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $signature = 'sitemap:generate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate the sitemap.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $path = public_path('sitemap.xml');
+
+        SitemapGenerator::create(config('app.url'))
+            ->getSitemap()
+            ->writeToFile($path);
+        
+        echo "Sitemap generated.";
+    }
+}
